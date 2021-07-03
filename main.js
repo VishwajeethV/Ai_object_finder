7b3647786044c981614eb3774f76633be96487c8
VishwajeethV <spacexblue80@gmail.com>
2021-07-03 19:27:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 status="";
 objects=[];
-Otbf="";
+otbf="";
 
 function setup() {
     canvas=createCanvas(400,300);
@@ -12,31 +12,33 @@ function setup() {
 function modelloaded() {
     console.log("model is loaded");
     status= true;
-    objectdetector.detect(video,getResult);
 }
 
-function search() {
+
+function start() {
     objectdetector=ml5.objectDetector("cocossd",modelloaded);
     document.getElementById("status").innerHTML="Status - Detecting Objects";
     otbf=document.getElementById("input").value;
-
 }
 
+
 function getResult(error,results) {
-    if(error) {
-        console.log(error);
-    }
-    else{
+    if(results) {
         console.log(results);
         objects=results;
     }
+   else if (error) {
+        console.log(error);
+   } 
 }
 
 function draw() { 
-    image(video,0,0,700,500);
+    image(video,0,20,700,500);
 
     if(status!="") {
+        objectdetector.detect(video,getResult);
         for(var i=0;i<objects.length;i++) {
+            document.getElementById("status").innerHTML="object detected"
              fill("red");
              textSize(20);
              percent=floor(objects[i].confidence*100)
@@ -45,12 +47,19 @@ function draw() {
              stroke("red");
              strokeWeight(5);
              rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+             if (objects[i].label==otbf){
+                document.getElementById("ofn").innerHTML="Object found";
+          }
+            else{
+                 document.getElementById("ofn").innerHTML="object not found"
+            }
         }
     }
-
-
-
-
-
-
 }
+
+
+
+
+
+
